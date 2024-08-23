8ae5b22c3501a99a4aeb401dea70be6c5e84a108
Neil <neil.kangaroo@gmail.com>
2024-08-23 09:13:35 -0500

    Updated seeder files to debug render.com, ordered seeder functions appropriately

--- migrations/versions/20240822_180758_updated_models.py
+++ migrations/versions/20240822_180758_updated_models.py
@@ -8,7 +8,6 @@ Create Date: 2024-08-22 18:07:58.990720
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '4659ba6aad77'
 down_revision = '089a021d2f25'
