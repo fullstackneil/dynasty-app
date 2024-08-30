30c815e62a920277de964396efd19d9b3b31ea02
Neil <neil.kangaroo@gmail.com>
2024-08-30 15:30:07 -0500

    Added image_url as a field in SignUpForm(flask)

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, Length, ValidationError
+from wtforms.validators import DataRequired, Email, Length, ValidationError, Optional, URL
 from app.models import User
 
 
@@ -24,6 +24,7 @@ class SignUpForm(FlaskForm):
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
     phone_number = StringField('phone_number', validators=[DataRequired(), Length(max=10)])
+    image_url = StringField('image_url', validators=[Optional(), URL(message='Invalid URL')])
     username = StringField(
         'username', validators=[DataRequired(), Length(min=4, max=50), username_exists])
     email = StringField('email', validators=[DataRequired(), Email(), user_exists])
