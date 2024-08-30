4ffe3a8cc8fdb837c7e6cbc525fd6759fd6b2b44
Neil <neil.kangaroo@gmail.com>
2024-08-30 00:51:45 -0500

    Homepage login form complete, demo user login added

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
+import logo from '../../../assets/dynasty-app.svg'
 import "./LoginForm.css";
 
 function LoginFormPage() {
@@ -10,6 +11,7 @@ function LoginFormPage() {
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [ demo, setDemo ] = useState(false)
   const [errors, setErrors] = useState({});
 
   if (sessionUser) return <Navigate to="/" replace={true} />;
@@ -27,17 +29,43 @@ function LoginFormPage() {
     if (serverResponse) {
       setErrors(serverResponse);
     } else {
-      navigate("/");
+      navigate("/leagues");
     }
   };
 
+
+  const handleDemoUser = async (e) => {
+    e.preventDefault();
+    setDemo(true);
+
+    if (demo) {
+      const demoCredentials = {
+        email: "john.doe@example.com",
+        password: "password"
+      }
+
+      const serverResponse = await dispatch(thunkLogin(demoCredentials))
+
+      if (serverResponse) {
+        setErrors(serverResponse);
+      } else {
+        navigate('/leagues');
+      }
+
+      setErrors({})
+      setDemo(false)
+
+    }
+  }
+
   return (
     <>
-      <h1>Log In</h1>
+      {/* <h1>Log In</h1> */}
       {errors.length > 0 &&
-        errors.map((message) => <p key={message}>{message}</p>)}
+        errors.map((message) => <p key={message} className='error-message'>{message}</p>)}
       <form onSubmit={handleSubmit} className="form-content">
-        <label>
+        <img src={logo} alt="Dynasty App Logo" className="logo" />
+        <label className='email-text'>
           Email
           <input
             type="text"
@@ -47,8 +75,8 @@ function LoginFormPage() {
             className='input-text'
           />
         </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
+        {errors.email && <p className='error-message'>{errors.email}</p>}
+        <label className='credential-text'>
           Password
           <input
             type="password"
@@ -58,8 +86,13 @@ function LoginFormPage() {
             className='password-text'
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
+        {errors.password && <p className='error-message'>{errors.password}</p>}
         <button type="submit" className='button-text'>Log In</button>
+        <button className='demo-user-container' id='demo-user-container'
+          type='submit'
+          onClick={handleDemoUser}
+          >Log in as Demo User
+        </button>
       </form>
     </>
   );
